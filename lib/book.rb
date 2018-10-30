fd93b1c33c0d7ac907ff07c31ef5ff5343d00b3d
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-30 23:01:15 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -25,11 +25,14 @@ class Book
 
   def genre=(genre)
     @genre = genre
-  end 
+  end
 
   def genre
     @genre
-  end 
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
 
 end
